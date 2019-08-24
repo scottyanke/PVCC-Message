c877cee2d535b84f01779cbc74c5d65e61fd1a1a
scottyanke <syanke1959@gmail.com>
2019-08-24 09:22:29 -0500

    Initial setup
    Signed-off-by: scottyanke <syanke1959@gmail.com>

--- btserial.sh
+++ btserial.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -e
 
+# This script is used to enable controlling the headless Raspberry
+# using a serial bluetooth connection from a phone.  It is a backup
+# given that IP addresses are assigned dynamically (in this case) and
+# occastionally some type of remote access is needed to the Pi.  This
+# at least gets a user the ability to see what the IP address is so
+# that some other console connection can be used for diagnostics.
+
 #Edit the display name of the RaspberryPi so you can distinguish
 #your unit from others in the Bluetooth console
 #(very useful in a class setting)
 
-echo PRETTY_HOSTNAME=raspberrypi > /etc/machine-info
+echo PRETTY_HOSTNAME=message > /etc/machine-info
 
 # Edit /lib/systemd/system/bluetooth.service to enable BT services
 sudo sed -i: 's|^Exec.*toothd$| \
